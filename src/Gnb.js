6a811abd2daf6d9ddfdb5a12fb20c031b958d5ee
kownhee <qkrrnjsgml7@naver.com>
2024-01-14 23:11:06 +0900

    [feat] add gnb

---
+++ src/Gnb.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faUser, faTags, faT, faPen, faHashtag } from '@fortawesome/free-solid-svg-icons';
+
+function Gnb()
+{
+    return (
+        <>
+         <div className="gnb">
+            <Link to="/">
+                <FontAwesomeIcon icon={faHome}/> 
+                Home
+            </Link>
+            <Link to="/">
+                <FontAwesomeIcon icon={faHashtag}/> 
+                Magazine
+            </Link>
+            <Link to="/">
+                <FontAwesomeIcon icon={faT}/> 
+                Test
+            </Link>
+            <Link to="/mypage">
+                <FontAwesomeIcon icon={faUser}/> 
+                Mypage
+            </Link>
+        </div>
+        
+        </>
+    )
+}
+
+export default Gnb
