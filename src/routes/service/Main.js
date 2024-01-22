93d810775239fa8663cdf2274a777dbd1291e65d
kownhee <qkrrnjsgml7@naver.com>
2024-01-23 01:46:25 +0900

    [feat] Published main page
    plan concept design

--- src/routes/service/Main.js
+++ src/routes/service/Main.js
@@ -13,7 +13,52 @@ function Main()
     return (
         <>
         <div className="main">
-    
+
+        <section className='main-content'>
+            <div className='container'>
+                <div className='row'>
+                    <div className="col-md-3">
+                        <p className="keyword">다이어리</p>
+                    </div>
+                    <div className="col-md-3 ">
+                        <p className="keyword">달력</p>
+
+                    </div>
+                    <div className="col-md-3">
+                        <p className="keyword">노트</p>
+                    </div>
+                    <div className="col-md-3">
+                        <p className="keyword">영감</p>
+                    </div>
+                </div>
+
+                <div className="row mt-5">
+                    <div className="col-md-6">
+                        <div className="contentBox">
+                            
+                        </div>
+                    </div>
+                    <div className="col-md-6">
+                        <div className="contentBox">
+                            
+                        </div>
+                    </div>
+                </div>
+
+                <div className="row mt-5">
+                    <div className="col-md-6">
+                        <div className="contentBox">
+                            
+                        </div>
+                    </div>
+                    <div className="col-md-6">
+                        <div className="contentBox">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </section>
         </div>
         </>
     )
